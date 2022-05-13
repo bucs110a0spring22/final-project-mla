9f50f469ad09e90cac5aef6ff4da0e9fc26deaf9
Andrew Wang <98830902+aw134123@users.noreply.github.com>
2022-05-13 00:42:51 +0000

    replit is freaking out so im scared

--- src/controller.py
+++ src/controller.py
@@ -34,6 +34,7 @@ class Controller:
         y += word_height  # Start on new row.
     
   def gameloop(self):
+    has_result_text = False
     query = input("What do you want to search for? ")
     while self.state == "run":
       self.display.fill((250,250,250))
@@ -43,31 +44,27 @@ class Controller:
       self.display.blit(text, (70,140))
       self.display.blit(self.metabrowser.image, (30,10))
       pygame.display.flip()
-      result_text = ''
       for event in pygame.event.get():
         if event.type == pygame.MOUSEBUTTONDOWN:
           if self.searchbar.rect.collidepoint(event.pos):
+            result_text = ''
             print('beforeScraping')
             for scrape in [masterscraper.Googlescraper(), masterscraper.Bingscraper()]:
               result_filename = query + scrape.search_engine() + "results" + ".txt"
               search_results = scrape.search(query)
               scrape.save_results(search_results, result_filename)
-              result_file_handle = open(result_filename, "r")
-              search_t += f'{scrape.search_engine()} Result: \n{result_file_handle.read()}'
-
-      pygame.display.update()
-      font = pygame.font.SysFont('Arial', 4)
-      self.blit_text(self.display, result_text, (0, 150), font)
               
+              result_file_handle = open(result_filename, "r")
+              result_text += f'{scrape.search_engine()} Result: \n{result_file_handle.read()}'
+              has_result_text = True
+      
+      if has_result_text:
+        font = pygame.font.SysFont('Arial', 4)
+        self.blit_text(self.display, result_text, (0, 150), font)
+        pygame.display.update()        
               
                            
-            '''scrape = masterscraper.Masterscraper()
-            google_search_results = scrape.google_cse_search(query)
-            google_result_filename = query + "google" + "results" + ".txt"
-            scrape.google_save_results(google_search_results, google_result_filename)    
-            bing_search_results = scrape.bing_search(query)
-            bing_result_filename = query + "bing" + "results" + ".txt"
-            scrape.bing_save_results(bing_search_results, bing_result_filename)'''
+                      
             
             
             
